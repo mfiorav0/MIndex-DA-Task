d3a8011eed7938d78bd5f02e18f154aa9e79ca5b
mfiorav0 <59876268+mfiorav0@users.noreply.github.com>
2023-01-31 21:48:19 -0500

    Python file and SQL Code for Mindex DA Task

---
+++ Bengals_Lost_by_Three_VerySad.sql
@@ -0,0 +1,4 @@
+
+select sum("Boyd Yards") as "Boyd Yards", sum("Higgins Yards") as "Higgins Yards", sum("Chase Yards") as "Chase Yards", 
+count(case "Result" when 'Win' then 1 else null end)||'-'||count(case "Result" when 'Loss' then 1 else null end) as "Win/Loss"
+from matthew_fioravanti mf 
