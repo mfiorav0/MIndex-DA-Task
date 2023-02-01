01771d776d60e4ff6d682b9b186c13baee0c81ba
mfiorav0 <59876268+mfiorav0@users.noreply.github.com>
2023-01-31 21:53:47 -0500

    Update Bengals_Lost_by_Three_VerySad.sql
    Added comment at end of query

--- Bengals_Lost_by_Three_VerySad.sql
+++ Bengals_Lost_by_Three_VerySad.sql
@@ -2,3 +2,4 @@
 select sum("Boyd Yards") as "Boyd Yards", sum("Higgins Yards") as "Higgins Yards", sum("Chase Yards") as "Chase Yards", 
 count(case "Result" when 'Win' then 1 else null end)||'-'||count(case "Result" when 'Loss' then 1 else null end) as "Win/Loss"
 from matthew_fioravanti mf 
+--Wrote this script together, which is why there aren't many commits :(
